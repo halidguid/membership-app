84f732c4860a2355eeeedd0d93ec31117f7f7037
halidguid <halidhosi@gmail.com>
2021-11-27 16:07:13 +0100

    da

---
+++ mern_backend/src/config/config.js
@@ -0,0 +1,15 @@
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 5000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUrl:
+    process.env.MONGODB_URL ||
+    process.env.MONGO_HOST ||
+    "mongodb://" +
+      (process.env.IP || "localhost") +
+      ":" +
+      (process.env.MONGO_PORT || "27017") +
+      "/mernproject",
+};
+
+export default config;
---
+++ mern_backend/template.js
@@ -0,0 +1,11 @@
+export default () => {
+  return `<!doctype html>
+    <html lang=en>
+        <head>
+            <meta charset="utf-8>
+            <title>MERN skeleton</title>
+        </head>
+        <body>
+            <div id="root">Hello MERN</div>
+        </body>`;
+};
